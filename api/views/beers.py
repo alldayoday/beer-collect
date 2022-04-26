178e26158ef3db8f9a6bf48a417ad8f30d515625
Erin ODea <odea.erin@gmail.com>
2022-04-26 11:38:54 -0500

    adding, indexing, updating, deleting beers

--- api/views/beers.py
+++ api/views/beers.py
@@ -16,3 +16,42 @@ def create():
   db.session.add(beer)
   db.session.commit()
   return jsonify(beer.serialize()), 201
+
+@beers.route('/', methods=["GET"])
+def index():
+  beers = Beer.query.all()
+  return jsonify([beer.serialize() for beer in beers]), 200
+
+@beers.route('/<id>', methods=["GET"])
+def show(id):
+  beer = Beer.query.filter_by(id=id).first()
+  beer_data = beer.serialize()
+  return jsonify(beer=beer_data), 200
+
+@beers.route('/<id>', methods=["PUT"]) 
+@login_required
+def update(id):
+  data = request.get_json()
+  profile = read_token(request)
+  beer = Beer.query.filter_by(id=id).first()
+
+  if beer.profile_id != profile["id"]:
+    return 'Forbidden', 403
+  for key in data:
+    setattr(beer, key, data[key])
+
+  db.session.commit()
+  return jsonify(beer.serialize()), 200
+
+@beers.route('/<id>', methods=["DELETE"]) 
+@login_required
+def delete(id):
+  profile = read_token(request)
+  beer = Beer.query.filter_by(id=id).first()
+
+  if beer.profile_id != profile["id"]:
+    return 'Forbidden', 403
+
+  db.session.delete(beer)
+  db.session.commit()
+  return jsonify(message="Success"), 200
